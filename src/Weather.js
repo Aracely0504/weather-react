3e4fcfbe2445e08e34edd4f58ab3bcd85e149c31
Aracely0504 <aracelybc90@gmail.com>
2025-01-21 21:00:18 -0800

    Fixed warning issue

--- src/Weather.js
+++ src/Weather.js
@@ -8,7 +8,7 @@ export default function Weather(props) {
       `The weather in ${response.data.name} is ${response.data.main.temp}°C`
     );
   }
-     let apiKey = "8cac06f7ab6c10287cd06a316ff84a57";
-   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-   
- 
+}
+
+let apiKey = "8cac06f7ab6c10287cd06a316ff84a57";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
